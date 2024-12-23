7b3122833e6f7567bf0bbc8a5490d6f7363ba1ab
nekit1338 <tr1n1nG@inbox.ru>
2024-12-23 22:24:47 +0500

    Добавил список использованных команд в файл 'models.py'

--- module19/task1/models.py
+++ module19/task1/models.py
@@ -35,3 +35,28 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
+
+
+#  """Список использованных команд в shell"""
+#
+#  "Добавление покупателей в таблицу Buyer"
+#
+#  first_buyer = Buyer.objects.create(name="Ilya", balance=1500.05, age=24)
+#  second_buyer = Buyer.objects.create(name="Terminator2000", balance=42.15, age=52)
+#  third_buyer = Buyer.objects.create(name="Ubivator432", balance=0.5, age=16)
+#
+#  "Добавление игр в таблицу Game"
+#
+#  first_game = Game.objects.create(title="Cyberpunk 2077", cost=31, size=46.2, description="Game of the year",
+#                                   age_limited=True)
+#  second_game = Game.objects.create(title="Mario", cost=5, size=0.5, description="Old Game", age_limited=False)
+#  third_game = Game.objects.create(title="Hitman", cost=12, size=36.6, description="Who kills Mark?", age_limited=True)
+#
+#  "Связывание Buyer и Game"
+#
+#  first_buyer.games.set([first_game, second_game, third_game])  # У первого покупателя есть все игры
+#
+#  second_game.buyers.add(second_buyer)  # У второго 2 игры
+#  third_game.buyers.add(second_buyer)
+#
+#  third_buyer.games.add(second_game)  # Третий покупатель (младше 18) имеет только одну игру "Mario"
